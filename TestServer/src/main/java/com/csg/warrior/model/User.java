9186e1258ebbd03a17cf6f3c4bece3ed250f0967
John Smith Paraggua <smitparaggua@gmail.com>
2013-09-01 17:17:42 +0800

    Added Hibernate Configuration

---
+++ TestServer/src/main/java/com/csg/warrior/model/User.java
@@ -0,0 +1,67 @@
+package com.csg.warrior.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long userId;
+    @Column(unique=true)
+    private String username;
+    private String password;
+    private String mobileKey;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileKey() {
+        return mobileKey;
+    }
+
+    public void setMobileKey(String mobileKey) {
+        this.mobileKey = mobileKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return username != null ? username.hashCode() : 0;
+    }
+
+}
